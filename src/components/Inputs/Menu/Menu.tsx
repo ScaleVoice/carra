c9974df15c69c1452cf791459d9f718c6ae212c3
Evan Santa <evan.santa@cleevio.com>
2024-04-29 01:32:06 +0200

    Select gap

--- src/components/Inputs/Menu/Menu.tsx
+++ src/components/Inputs/Menu/Menu.tsx
@@ -34,7 +34,7 @@ export const Menu = forwardRef<HTMLDivElement, Props>(
 
             <Listbox.Button
               className={twMerge(
-                "flex items-center justify-between gap-1 rounded-lg border border-gray-50 px-4 py-2.5 text-gray",
+                "flex items-center justify-between gap-2 rounded-lg border border-gray-50 px-4 py-2.5 text-gray",
                 wrapperStateVariants,
                 open && "rounded-b-none",
                 placeholder && !value && "text-gray-400",
